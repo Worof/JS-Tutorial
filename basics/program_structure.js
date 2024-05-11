d8d4db24a9a9d37addccd9c811f187b956835d30
Worof <worof.eng_5760@eng.kfs.edu.eg>
2024-05-11 22:52:39 +0200

    some examples on JS program structure

--- basics/program_structure.js
+++ basics/program_structure.js
@@ -120,3 +120,11 @@ do {
     var yourName = prompt (" Who are you ?") ;
     } while (! yourName ) ;
     console . log ( yourName ) ;
+
+
+    /*IMPORTANT NOTE: This program will force you to enter a name. It will ask again and
+again until it gets something that is not an empty string. Applying
+the ! operator will convert a value to Boolean type before negating it,
+and all strings except "" convert to true. This means the loop continues
+going round until you provide a name that is not the empty string.
+*/
