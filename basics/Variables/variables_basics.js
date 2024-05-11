27cb6afe884fe3a5fdccab8c54b19651b4c5724b
Worof <worof.eng_5760@eng.kfs.edu.eg>
2024-05-11 04:40:02 +0200

    th basics of values in JS and using them

---
+++ basics/Variables/variables_basics.js
@@ -0,0 +1,29 @@
+var ten = 10;
+console.log(ten);
+//-->10
+console.log(ten * ten);
+// → 100
+
+var mood = " light ";
+console.log(mood);
+// → light
+mood = " dark ";
+console.log(mood);
+// → dark
+
+var luigisDebt = 140;
+luigisDebt = luigisDebt - 35;
+console.log(luigisDebt);
+// → 105
+
+var one = 1 , two = 2;
+console . log ( one + two ) ;
+// -> 3
+
+
+/*A lot of the values provided in the default environment have the type
+function. A function is a piece of program wrapped in a value. Such
+values can be applied in order to run the wrapped program. For example, in a browser environment, the variable alert holds a function that
+shows a little dialog box with a message. It is used like this:*/
+
+alert (" Good morning !") ; //Executing a function is called invoking, calling, or applying it. 
