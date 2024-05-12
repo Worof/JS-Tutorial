998ed5a2eeac4373bb9b8f36586935a96ca58f5d
Worof <worof.eng_5760@eng.kfs.edu.eg>
2024-05-12 02:29:16 +0200

    Some examples on the function in JS

--- functions/script.js
+++ functions/script.js
@@ -3,9 +3,63 @@
 /*A function definition is just a regular variable definition where the value
 given to the variable happens to be a function*/
 
-var square = function ( x ) {
-    return x * x ;
-    };
-    console . log ( square (12) ) ;
-    // → 144
-    
+var square = function (x) {
+    return x * x;
+};
+console.log(square(12));
+// → 144
+
+
+/*A function is created by an expression that starts with the keyword
+function with a set of parameters and a body*/
+
+var makeNoise = function () {
+    console.log(" Pling !");
+};
+makeNoise();
+// → Pling !
+var power = function (base, exponent) {
+    var result = 1;
+    for (var count = 0; count < exponent; count++){
+        result *= base;
+       
+    }
+    return result;
+
+};
+console.log(power(2, 10));
+// → 1024
+/*NOTE: Some functions produce a value, such as power and square, and some don’t,
+such as makeNoise, which produces only a side effect.*/
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////****************************************************
+/////////////////////////////////////////PARAMETERS AND SCOPE************////////////////////////////////////////////////////////
+
+/*The parameters to a function behave like regular variables, but their
+initial values are given by the caller of the function, not the code in the
+function itself.
+*/
+
+//The difference between global and local variables in the function scope//
+
+var x = " outside ";
+var f1 = function () {
+var x = " inside f1 ";
+};
+f1 () ;
+console . log ( x ) ;
+// → outside
+var f2 = function () {
+x = " inside f2 ";
+};
+f2 () ;
+console . log ( x ) ;
+// → inside f2
+
